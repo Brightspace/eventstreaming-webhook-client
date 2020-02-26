ce845f9372029a41ca8d67b4591305ffb06a72c7
Benyamin Noori <Benyamin.Noori@D2L.com>
2020-02-26 11:46:02 -0500

    new dependency and lock file

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 const express = require('express');
 const app = express();
+const sha256 = require('crypto-js/sha256');
+const Base64 = require('crypto-js/enc-base64');
 
+const secret = 'cifVxv4OVCt4WkXOeFEeGGPyFdoUG1t2';
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.all('',function(req,res) {
-  console.log(req.headers);
-  console.log(req.body);
-  const brightspaceEvents = req.body.Records;
-  console.log(`Successfully received ${brightspaceEvents.length} events.`);
-  res.send('OK');
-  process(brightspaceEvents);
+app.all('', function(req,res) {
+	console.log(req.headers);
+	console.log(req.body);
+	const computedHeader = computeHeader(
+		secret,
+		JSON.stringify(req.body)
+	);
+	const requestHeader = req.headers['x-brightspace-hmac-sha256'];
+	if ( requestHeader !== computedHeader ) {
+		console.log(`Expected ${computedHeader} but received ${requestHeader}`);
+		throw new Error('Bad request.');
+	}
+	res.send('OK');
+	const brightspaceEvents = req.body.Records;
+	console.log(`Successfully received ${brightspaceEvents.length} events.`);
+	process(brightspaceEvents);
 });
 
 const process = (events) => {
-  for (const event of events) {
-    console.log(JSON.stringify(event));
-  }
-}
+	for (const event of events) {
+		console.log(JSON.stringify(event));
+	}
+};
+
+const computeHeader = (key, message) => {
+	const digest = sha256(key + sha256(key + message));
+	return Base64.stringify(digest);
+};
 
 module.exports = app;
